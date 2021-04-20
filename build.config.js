c7aa5d97166ecdf4e99003e5f19e0991a8dbc962
jxl <57953279@qq.com>
2021-04-20 16:17:51 +0800

    1

--- build.config.js
+++ build.config.js
@@ -13,11 +13,17 @@ module.exports = {
         {name: 'vue-router', scope: 'VueRouter'},
         {name: 'vuex', scope: 'Vuex'},
         {name: 'axios', scope: 'axios'},
+        {name: 'vant', scope: 'vant'},
         {name: 'dayjs', scope: 'dayjs', path: '/dayjs/1.4.1/dayjs.min.js'},
         {name: 'nanoid', scope: 'nanoid', path: '/nanoid/3.1.0/async/index.min.js'},
         {name: 'vue-clipboard2', scope: 'VueClipboard', path: '/vue-clipboard2/0.0.6/vue-clipboard.min.js'},
         {name: 'vue-i18n', scope: 'Vue18n', path: '/vue-i18n/0.1.2/vue-i18n.min.js'},
-        {name: 'vuex-persistedstate', scope: 'createPersistedState', path: '/vuex-persistedstate/2.5.0/vuex-persistedstate.min.js'},
+        {
+            name: 'vuex-persistedstate',
+            scope: 'createPersistedState',
+            path: '/vuex-persistedstate/2.5.0/vuex-persistedstate.min.js'
+        },
+        {name: 'crypto-js', scope: 'CryptoJS'}
 
     ]
 };
