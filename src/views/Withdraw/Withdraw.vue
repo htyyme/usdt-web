197b86a071d37079abaf261a9b16d81b33c61320
jxl <57953279@qq.com>
2021-04-22 15:11:09 +0800

    1

--- src/views/Withdraw/Withdraw.vue
+++ src/views/Withdraw/Withdraw.vue
@@ -8,7 +8,9 @@
 
     <van-notice-bar scrollable :text="$t('handlefeetip',{num:0.18})" color="#CF182C" background="#e5e5e5"/>
     <bankCardInfo />
+    <pageFooter />
 
+    <van-button  block  class="withdraw-btn" >{{$t('Withdraw')}}</van-button>
   </div>
 </template>
 
@@ -17,13 +19,15 @@ import assets from "./cpns/assets";
 import record from "./cpns/record";
 import withdrawForm from "./cpns/withdrawForm";
 import bankCardInfo from "./cpns/bankCardInfo";
+import pageFooter from "./cpns/pageFooter";
 export default {
   name: "Withdraw",
   components:{
     assets,
     record,
     withdrawForm,
-    bankCardInfo
+    bankCardInfo,
+    pageFooter
   }
 }
 </script>
@@ -32,10 +36,21 @@ export default {
 .Withdraw {
   min-height: 100vh;
   background-color: #f4f4f4;
+  padding-bottom: 35px;
 
   .van-notice-bar {
     font-size: 12px;
     height: 25px;
+  }
+
+  .withdraw-btn {
+    width: 250px;
+    height: 55px;
+    background: linear-gradient(180deg, rgba(94, 217, 248, 0.99) 0%, rgba(29, 111, 223, 0.99) 100%);
+    border-radius: 28px;
+    font-size: 20px;
+    color: #fff;
+    margin: 20px auto 0;
   }
 }
 
---
+++ src/views/Withdraw/cpns/pageFooter.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="pageFooter">
+    <div class="card-wrapper tip">
+      <p v-html="config.withdraw_tips1"></p>
+    </div>
+
+    <div class="time">
+      <em>{{$t('Withdrawal Time')}}</em>
+      <span>{{$t('Working day')}} {{config.withdraw_time}}</span>
+    </div>
+    <div class="end" v-html="config.withdraw_tips2"></div>
+
+  </div>
+</template>
+
+<script>
+
+    export default {
+      name: "pageFooter",
+      computed:{
+        config(){
+          return this.$store.getters['system/config']
+        }
+      }
+    }
+</script>
+
+<!--suppress CssUnknownTarget -->
+<style lang="scss" scoped>
+  @import "~assets/css/vars";
+
+  .card-wrapper{
+    display: block;
+    width: 345px;
+    background-color: #fff;
+    margin: 15px auto;
+    border-radius: 10px;
+    padding: 11px ;
+    .card-tit{
+      font-weight: 700;
+      font-size: 17px;
+    }
+    .card-val{
+      //color: $mainColor;
+      height: 49px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 21px;
+      background-color: #F6F2FC;
+      border-radius: 50px;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      overflow: hidden;
+      input{
+        flex: 1;
+        height: 100%;
+        border:none;
+        background-color: transparent;
+        font-size: 14px;
+        padding-left: 26px;
+        border-radius: 50px;
+        &::placeholder{
+          color: #999;
+        }
+      }
+      .van-button{
+        margin-left: 10px;
+      }
+    }
+
+    dl{
+      dt{
+        font-weight: 700;
+        font-size: 17px;
+        color: #97B9F5;
+      }
+      dd{
+        height: 49px;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        font-size: 17px;
+        background-color: #F9F9F9;
+        border-radius: 50px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        overflow: hidden;
+        padding-left: 26px;
+      }
+    }
+  }
+
+  .pageFooter {
+    .tip {
+      font-size: 13px;
+      background-color: #C9E1FF;
+      color: #1849A1;
+
+      p {
+        margin: 8px 0;
+      }
+    }
+
+    .time {
+      width: 345px;
+      height: 45px;
+      display: flex;
+      align-items: center;
+      font-size: 13px;
+      background-color: #e6e6e6;
+      border-radius: 22px;
+      margin: 0 auto;
+      padding: 0 18px;
+      font-weight: 700;
+
+      em {
+        color: $mainColor;
+      }
+
+      span {
+        margin-left: 15px;
+      }
+    }
+
+    .end {
+      font-size: 12px;
+      color: #999;
+      padding: 10px 20px;
+    }
+  }
+</style>
