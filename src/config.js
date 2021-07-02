c9f820ddee7bf39e3a8d67b1a49e64881fb615de
jxl <57953279@qq.com>
2021-07-02 18:50:48 +0800

    1

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@
 const IS_APP = false
 
 export default {
-    env: 'dev',//dev/prod
+    env: 'prod',//dev/prod
     isApp: IS_APP,
     apkVersion: '1.0.3', //版本
-    appName: 'jdmoney',//网站标题
+    appName: 'vicaro',//网站标题
     host: 'http://vicaro.in/',//网站域名
     encryptKey: '4n3I6Hni50YsyUgTix0CL01Z',
     locale: 'en',//默认语言
