363d1238a5963d0257fe8e099b7dd6d3614e1e92
xiaofei <xiaofei@qq.com>
2021-07-09 15:05:09 +0800

    更新印度时区

--- src/config.js
+++ src/config.js
@@ -1,15 +1,16 @@
 //是否为APP
-const IS_APP = false
+const IS_APP = true
 
 export default {
     env: 'prod',//dev/prod
     isApp: IS_APP,
-    apkVersion: '1.0.1', //版本
+    apkVersion: '1.0.2', //版本
     appName: 'vicaro',//网站标题
-    host: 'https://app.vicaro.in/',//网站域名
+    host: 'https://www.vicaro.in/',//网站域名
     encryptKey: '4n3I6Hni50YsyUgTix0CL01Z',
     locale: 'en',//默认语言
-    timezone: 'Asia/Bangkok', //时区
+    // timezone: 'Asia/Bangkok', //时区 泰国
+    timezone: 'Asia/Kolkata', //时区 印度
     //接口地址
     baseApiUrl: IS_APP ? 'https://app.vicaro.in/' : '/'
 }
