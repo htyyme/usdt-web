d47037fa91cfdf5e78173e6e3545bd300eb37e5c
xiaofei <xiaofei@qq.com>
2021-07-12 11:21:55 +0800

    version1.0.3

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 //是否为APP
-const IS_APP = false
+const IS_APP = true
 
 export default {
     env: 'prod',//dev/prod
     isApp: IS_APP,
-    apkVersion: '1.0.2', //版本
+    apkVersion: '1.0.3', //版本
     appName: 'vicaro',//网站标题
     host: 'https://www.vicaro.in/',//网站域名
     encryptKey: '4n3I6Hni50YsyUgTix0CL01Z',
