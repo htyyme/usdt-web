b352f61727a4b3e209296375d4fc04204a54387e
jxl <57953279@qq.com>
2021-12-27 15:20:37 +0800

    1

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default {
     apkVersion: '1.0.0', //版本
     appName: 'bitcoiniojo',//网站标题
     host: 'https://www.bitcoiniojo.com/',//网站域名
-    locale: 'en',//默认语言
+    locale: 'English',//默认语言
     // timezone: 'Asia/Bangkok', //时区 泰国
     timezone: 'Asia/Kolkata', //时区 印度
     //接口地址
