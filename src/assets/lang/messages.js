8cecfd9e9ff8605f74ba223f1d03a4215da4acf1
king <fullvip@gmail.com>
2022-01-26 10:44:42 +0800

    更新翻译文件

--- src/assets/lang/messages.js
+++ src/assets/lang/messages.js
@@ -1,9 +1,11 @@
 import en from './en'
 import th from './th'
 import spanish from './spanish.js'
+import turkey from './Turkey.js'
 
 export default {
     'English':en,
     'Thailand':th,
-    'Spanish':spanish
+    'Spanish':spanish,
+    'Turkey':turkey
 }
--- src/config.js
+++ src/config.js
@@ -9,16 +9,15 @@ export default {
     apkVersion: '1.0.0', //版本
     appName: 'TRY',//网站标题
     host: 'https://app.bitcoiniojo.com/',//网站域名
-    locale: 'Spanish',//默认语言  Spanish 西班牙语
+    locale: 'Turkey',//默认语言  Spanish 西班牙语  Turkey 土耳其语
+    //时区查询https://jp.cybozu.help/general/zh/admin/list_systemadmin/list_localization/timezone.html
     // timezone: 'Asia/Bangkok', //时区 泰国
     // timezone: 'Asia/Kolkata', //时区 印度
-    timezone: 'America/New_York', //时区 美东时间 -5 时区
+    // timezone: 'America/New_York', //时区 美东时间 -5 时区
+    timezone: 'Europe/Minsk', //时区 +3 时区
     //接口地址
     baseApiUrl: IS_APP ? 'https://app.bitcoiniojo.com/' : '/'
 }
 
-
 // 正式版上线需要修改 usdt-web/src/store/module/system.js  将 config.three_service_address = '1'  加上就是正式版
-
-
 
