5b174967eb42328cd0a4dad20546ea6a08927b94
jxl <57953279@qq.com>
2022-03-22 10:12:30 +0800

    1

--- src/assets/lang/messages.js
+++ src/assets/lang/messages.js
@@ -1,9 +1,11 @@
 import en from './en'
 import th from './th'
 import spanish from './spanish.js'
+import Poland from "./Poland.js";
 
 export default {
     'English':en,
     'Thailand':th,
-    'Spanish':spanish
+    'Spanish':spanish,
+    'Poland':Poland,
 }
