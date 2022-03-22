f37b5777c478bdceeac89eb1101486e6f80e4ae2
jxl <57953279@qq.com>
2022-03-22 10:10:27 +0800

    1

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -1,6 +1,5 @@
 export default {
     'coin': '₺',
-    // vant
     tel: 'Phone',
     save: 'Save',
     confirm: 'Confirm',
@@ -10,7 +9,6 @@ export default {
     nameInvalid: 'Malformed name',
     confirmDelete: 'Are you sure you want to delete?',
     telInvalid: 'Malformed phone number',
-
     'lvMember': 'LV{num} MEMBER',
     "tabBarHome": 'home',
     "tabBarOrder": 'order',
@@ -106,9 +104,6 @@ export default {
     'WithdrawRecord': 'Withdraw Record',
     'pleaseEnterPhone': 'Please enter the phone number',
     'loading': 'loading',
-
-
-    //me
     'id': 'ID:',
     'membershipLevel': 'Membership level :',
     'lv': 'LV',
@@ -119,7 +114,6 @@ export default {
     'changeName': 'Change Name',
     'introduction': 'Introduction',
     'signOut': 'Sign out',
-    // bankCard
     'bankCardInfo': 'Bank Card Info',
     'UPINumber': 'UPI ID',
     'fillInPersonalInformation': 'Fill in personal information',
@@ -188,8 +182,6 @@ export default {
     'email': 'Email',
     'EnterEmail': 'Enter your email',
     'bankname': 'Bank Name',
-
-
     'vipInfo1': 'Each withdrawal limit is {num}',
     'vipInfo2': 'The number of orders can be swipe until {num} times',
     'vipInfo3': 'The promotion rebate is {num1}% {num2}% {num3}%',
@@ -206,8 +198,6 @@ export default {
     'successTip': 'if the green prompt "Succeeded" is displayed,the withdrawal is successful,please go to you bank account to confirm.',
     'failedTip': 'if the red prompt "Order failed" is displayed,the channel party may not receive the order or the bank processing has timed out.You can try to withdraw again or contact customer service to resove.',
     'withdrawTip': 'If the withdrawal is not arrived,please contact customer service as soon as possible to resove.',
-
-
     'WISH online order': 'WISH online order',
     'Invite more friends': 'Invite more friends',
     'Financial product': 'Financial product',
@@ -232,8 +222,6 @@ export default {
     'p17': 'if the red prompt "Order failed" is displayed, the channel party may not receive the order or the bank processing has timed out. You can try to withdraw again or contact customer service to resolve.',
     'p18': 'If You need help , please contact our online customer service',
     'p19': 'According to bank regulations,You can withdraw cash on working days(cannot withdraw cash on holidays)',
-
-
     'personalInfo': 'Personal Info',
     'Customer Service': 'Customer Service',
     'Back To Home': 'Back To Home',
@@ -248,7 +236,6 @@ export default {
     'Password length is at least 6 characters': 'Password length is at least 6 characters',
     'The two passwords are inconsistent': 'The two passwords are inconsistent',
     'Academy': 'Academy',
-
     'Pay': 'Pay',
     'Receiving account': 'Receiving account',
     'Beneficiary Bank': 'Beneficiary Bank',
@@ -667,24 +654,4 @@ export default {
     'red envelope record':'red envelope record',
     'Num':'Num',
     'Bonus':'Bonus',
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
