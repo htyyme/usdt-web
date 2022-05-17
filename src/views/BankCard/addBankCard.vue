49d31436c6e46061c3f9a6e557e4658c32df7b58
jxl <57953279@qq.com>
2022-05-17 10:28:28 +0800

    修改Venezuela版本银行卡

--- src/views/BankCard/addBankCard.vue
+++ src/views/BankCard/addBankCard.vue
@@ -7,18 +7,22 @@
 <script>
 import addBankCard_Columbia from "./addBankCard_Columbia.vue";
 import addBankCard_default from "./addBankCard_default.vue";
+import addBankCard_Venezuela from "./addBankCard_Venezuela.vue";
+
 
 export default {
   components:{
     addBankCard_Columbia,
-    addBankCard_default
+    addBankCard_default,
+    addBankCard_Venezuela
   },
   computed:{
     language(){
       return this.$store.getters['system/config'].language
     },
     cpn(){
-      return this.language === 'Columbia' ? 'addBankCard_Columbia' : 'addBankCard_default'
+      // return this.language === 'Columbia' ? 'addBankCard_Columbia' : 'addBankCard_default'
+      return 'addBankCard_Venezuela'
     }
   }
 }
