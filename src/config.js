9e1dd537b12f3fc0f07c88a347679b81e22428af
king <fullvip@gmail.com>
2022-11-04 15:49:17 +0800

    更新ENI配置参数

--- src/config.js
+++ src/config.js
@@ -3,22 +3,22 @@ const IS_APP = false
 const IS_GOOGLE = false
 
 export default {
-    env: 'dev',//dev/prod
+    env: 'prod',//dev/prod
     isApp: IS_APP,
     isGoogle: IS_GOOGLE,
     apkVersion: '1.0.0', //版本
-    appName: 'GMOCM',//网站标题
-    host: 'https://www.gmocm.com/',//网站域名
+    appName: 'ENI',//网站标题
+    host: 'https://www.enioilwell.com/',//网站域名
     locale: 'Spanish',//默认语言  Spanish 西班牙语  Turkey 土耳其语
     //时区查询https://jp.cybozu.help/general/zh/admin/list_systemadmin/list_localization/timezone.html
     // timezone: 'Asia/Bangkok', //时区 泰国
     // timezone: 'Asia/Kolkata', //时区 印度
-    // timezone: 'America/New_York', //时区 美东时间 -5 时区
+    timezone: 'America/New_York', //时区 美东时间 -5 时区
     // timezone: 'Europe/Minsk', //时区 +3 时区
     // timezone: 'America/Halifax', //时区 美东时间 -4 时区 智利
-    timezone: 'America/Caracas', //时区 美东时间 (UTC-04:30) 加拉加斯	 委内瑞拉
+    // timezone: 'America/Caracas', //时区 美东时间 (UTC-04:30) 加拉加斯	 委内瑞拉
     //接口地址
-    baseApiUrl: IS_APP ? 'https://www.gmocm.com/' : '/',
+    baseApiUrl: IS_APP ? 'https://www.enioilwell.com/' : '/',
     //是否开启交易所
     openExchange:false
 }
